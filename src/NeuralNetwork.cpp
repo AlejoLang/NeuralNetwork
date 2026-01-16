801fff10297ef09c7607abf70d0cd99511a61098
Alejo Lang <alejolang123@gmail.com>
2026-01-16 12:24:00 -0300

    Set each layer activation function and fix the delta set for the output layer on backwards

--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -2,18 +2,24 @@
 #include <cmath>
 #include <iostream>
 
-double times2(double x) {
-    return x * 2;
+template <typename T> T sigmoid(T x) {
+    return (1 / (1 + pow(M_E, -x)));
+}
+template <typename T> T softmax(T x) {
+    return (pow(M_E, -x) / pow((1 + pow(M_E, -x)), 2));
 }
 
 NeuralNetwork::NeuralNetwork(std::vector<int> layersConfig)
     : output(1, layersConfig[layersConfig.size() - 1]) {
     this->layersConfig = layersConfig;
-    for (size_t i = 1; i < layersConfig.size();
+    for (size_t i = 1; i < layersConfig.size() - 1;
          ++i) { // Creates the layers ignoring the first one since it doesnt need weights or biases
-        Layer newLayer(layersConfig[i], layersConfig[i - 1]);
+        Layer newLayer(layersConfig[i], layersConfig[i - 1], Layer::SIGMOID);
         this->layers.push_back(newLayer);
     }
+    Layer lastLayer(layersConfig[layersConfig.size() - 1], layersConfig[layersConfig.size() - 2],
+                    Layer::SOFTMAX);
+    this->layers.push_back(lastLayer);
 }
 
 void NeuralNetwork::randomize() {
@@ -23,19 +29,18 @@ void NeuralNetwork::randomize() {
 }
 
 Matrix<double> NeuralNetwork::foward(Matrix<double> input) {
-    for (Layer& layer : this->layers) {
-        input = layer.foward(input);
+    for (size_t i = 0; i < this->layers.size(); i++) {
+        input = this->layers[i].foward(input);
     }
     this->output = input;
     return input;
 }
 
 void NeuralNetwork::backwards(Matrix<double> target) {
-    Matrix<double> firstStepDeltas = (target - output) * 2;
-    Matrix<double> firstStepWeights(this->layers[this->layers.size() - 1].getNodeCount(),
-                                    this->layers[this->layers.size() - 1].getNodeCount());
-    Matrix<double> backwardsResultDeltas =
-        this->layers[this->layers.size() - 1].backwards(firstStepWeights, firstStepDeltas);
+    Matrix<double> firstStepDeltas = (output - target);
+    this->layers[this->layers.size() - 1].setDeltas(
+        firstStepDeltas); // Set the deltas of the output layer as the cost function
+    Matrix<double> backwardsResultDeltas = firstStepDeltas;
     for (int i = this->layers.size() - 2; i >= 0; --i) {
         backwardsResultDeltas =
             this->layers[i].backwards(this->layers[i + 1].getWeights(), backwardsResultDeltas);
