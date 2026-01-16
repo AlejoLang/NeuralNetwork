4f164af5d6f1fcfba47f7d34fc76a88d485f04a6
Alejo Lang <alejolang123@gmail.com>
2026-01-16 18:47:02 -0300

    Add batchsize parameter

--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -9,16 +9,16 @@ template <typename T> T softmax(T x) {
     return (pow(M_E, -x) / pow((1 + pow(M_E, -x)), 2));
 }
 
-NeuralNetwork::NeuralNetwork(std::vector<int> layersConfig)
-    : output(1, layersConfig[layersConfig.size() - 1]) {
+NeuralNetwork::NeuralNetwork(std::vector<int> layersConfig, int batchSize)
+    : output(batchSize, layersConfig[layersConfig.size() - 1]) {
     this->layersConfig = layersConfig;
     for (size_t i = 1; i < layersConfig.size() - 1;
          ++i) { // Creates the layers ignoring the first one since it doesnt need weights or biases
-        Layer newLayer(layersConfig[i], layersConfig[i - 1], Layer::SIGMOID);
+        Layer newLayer(layersConfig[i], layersConfig[i - 1], Layer::RELU, batchSize);
         this->layers.push_back(newLayer);
     }
     Layer lastLayer(layersConfig[layersConfig.size() - 1], layersConfig[layersConfig.size() - 2],
-                    Layer::SOFTMAX);
+                    Layer::SOFTMAX, batchSize);
     this->layers.push_back(lastLayer);
 }
 
@@ -49,6 +49,6 @@ void NeuralNetwork::backwards(Matrix<double> target) {
 
 void NeuralNetwork::update() {
     for (size_t i = 0; i < this->layers.size(); i++) {
-        this->layers[i].update(0.01);
+        this->layers[i].update(0.1);
     }
 }
